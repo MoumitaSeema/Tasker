da64e54b66f31e8e23c150b138cdf5015caca057
MoumitaSeema <moumitaseema385@gmail.com>
2024-01-29 20:35:25 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Header from "./Header";
+import HeroSection from "./HeroSection";
+import Footer from "./Footer";
+import TaskBoard from "./task/TaskBoard";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <div className="flex flex-col justify-center items-center">
+      <HeroSection />
+      <TaskBoard/>
+      </div>
+      <Footer />
+    </>
+  );
+};
+
+export default App;
